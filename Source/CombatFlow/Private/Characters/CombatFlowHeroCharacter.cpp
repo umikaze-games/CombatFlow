e1947af36109671516700f5f8aa30ca4aac89657
umikaze-games <c463384962@gmail.com>
2025-09-06 16:26:14 +0900

    commit

--- Source/CombatFlow/Private/Characters/CombatFlowHeroCharacter.cpp
+++ Source/CombatFlow/Private/Characters/CombatFlowHeroCharacter.cpp
@@ -3,6 +3,34 @@
 
 #include "Characters/CombatFlowHeroCharacter.h"
 #include "CombatFlowDebugHelper.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ACombatFlowHeroCharacter::ACombatFlowHeroCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(42,96);
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	CameraBoom=CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 200.0f;
+	CameraBoom->SocketOffset=FVector(0.0f,55.f,65.0f);
+	CameraBoom->bUsePawnControlRotation=true;
+
+	FollowCamera=CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom,USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation=false;
+
+	GetCharacterMovement()->bOrientRotationToMovement=true;
+	GetCharacterMovement()->RotationRate=FRotator(0.f,500.f,0.f);
+	GetCharacterMovement()->MaxWalkSpeed=400.f;
+	GetCharacterMovement()->BrakingDecelerationWalking=2000.f;
+	
+}
 
 // Called when the game starts or when spawned
 void ACombatFlowHeroCharacter::BeginPlay()
--- Source/CombatFlow/Public/Characters/CombatFlowHeroCharacter.h
+++ Source/CombatFlow/Public/Characters/CombatFlowHeroCharacter.h
@@ -6,11 +6,24 @@
 #include "CombatFlowBaseCharacter.h"
 #include "CombatFlowHeroCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class COMBATFLOW_API ACombatFlowHeroCharacter : public ACombatFlowBaseCharacter
 {
 	GENERATED_BODY()
+public:
+	ACombatFlowHeroCharacter();
 
 protected:
 	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> CameraBoom;
+
+	UPROPERTY(visibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> FollowCamera;
+	
 };
